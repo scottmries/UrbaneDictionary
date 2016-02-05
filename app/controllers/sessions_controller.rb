4957c92d0c9400fa362412cb676dbce4c21a9a4e
Scott Ries <scottmries@gmail.com>
2016-02-04 23:48:32 -0500

    Fix syntax error.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,6 +27,7 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_by_auth_hash(auth_hash)
     sign_in!(@user)
     redirect_to root_url + '#/'
+  end
 
   def destroy
     sign_out
